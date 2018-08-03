ed680f3c365948307b2ccd384b329d44bc7b79b8
yanghy <15737313694@163.com>
2018-08-03 11:11:10 +0800

    lombok

--- src/main/java/com/spring/aop/bean/User.java
+++ src/main/java/com/spring/aop/bean/User.java
@@ -1,7 +1,15 @@
 package com.spring.aop.bean;
 
-import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
+import java.io.Serializable;
+/*@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Accessors(chain = true)*/
 public class User implements Serializable{
     private static final long serialVersionUID = -5046972860468900599L;
 
